08575b69109979adc612149e8e5373d5b13feeb2
박준민 <min0214@tmon.co.kr>
2023-09-08 18:02:30 +0900

    lint, prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: ['standard-with-typescript', 'plugin:react/recommended', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react'],
+  rules: {},
+};
